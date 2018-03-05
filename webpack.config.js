82b172a2a8bc46c152981a75667ce9bba5761767
shawn <787990963@qq.com>
2018-03-05 11:15:52 +0800

    添加 optimization.splitChunks

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,54 @@
 const HtmlWebPackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader"
-        }
-      },
-      {
-        test: /\.html$/,
-        use: [
-          {
-            loader: "html-loader",
-            options: { minimize: true }
-          }
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                }
+            },
+            {
+                test: /\.html$/,
+                use: [
+                    {
+                        loader: "html-loader",
+                        options: { minimize: true }
+                    }
+                ]
+            }
         ]
-      }
+    },
+    optimization: {
+        splitChunks: {
+            chunks: "initial",         // 必须三选一： "initial" | "all"(默认就是all) | "async"
+            minSize: 0,                // 最小尺寸，默认0
+            minChunks: 1,              // 最小 chunk ，默认1
+            maxAsyncRequests: 1,       // 最大异步请求数， 默认1
+            maxInitialRequests: 1,    // 最大初始化请求书，默认1
+            name: () => { },              // 名称，此选项课接收 function
+            cacheGroups: {                 // 这里开始设置缓存的 chunks
+                priority: "0",                // 缓存组优先级 false | object |
+                vendor: {                   // key 为entry中定义的 入口名称
+                    chunks: "initial",        // 必须三选一： "initial" | "all" | "async"(默认就是异步)
+                    test: /react|lodash/,     // 正则规则验证，如果符合就提取 chunk
+                    name: "vendor",           // 要缓存的 分隔出来的 chunk 名称
+                    minSize: 0,
+                    minChunks: 1,
+                    enforce: true,
+                    maxAsyncRequests: 1,       // 最大异步请求数， 默认1
+                    maxInitialRequests: 1,    // 最大初始化请求书，默认1
+                    reuseExistingChunk: true   // 可设置是否重用该chunk（查看源码没有发现默认值）
+                }
+            }
+        }
+    },
+    plugins: [
+        new HtmlWebPackPlugin({
+            template: "./src/index.html",
+            filename: "./index.html"
+        })
     ]
-  },
-  plugins: [
-    new HtmlWebPackPlugin({
-      template: "./src/index.html",
-      filename: "./index.html"
-    })
-  ]
 };
